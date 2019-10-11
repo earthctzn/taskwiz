f650173028a966a0a6c6ba8c82d6da83af01a72e
earthctzn <earthctzn1@gmail.com>
2019-10-11 02:16:14 -0400

    Generating sessions, tasks, and users controllers with their corresponding helpers and views. Adding sessions routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users do
     resources :tasks, only: [:edit, :show]
   end
-
+  resources :sessions, only: [new, :create, :destroy]
   resources :tasks
   get '/auth/facebook/callback' => 'sessions#create'
 
