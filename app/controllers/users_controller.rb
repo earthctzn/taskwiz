9623f258871788c4bbc386cd1f43cb2a7deeff7c
earthctzn <earthctzn1@gmail.com>
2019-10-12 19:29:16 -0400

    Adding user_params, show, edit, index, and create methods to users_controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,26 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = user.new
   end
 
   def index
+    logged_in?
+    @user = User.all
   end
 
   def edit
+    logged_in?
+    current_user
   end
 
   def show
+    @user = user.find_by(id: params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :admin)
   end
 end
