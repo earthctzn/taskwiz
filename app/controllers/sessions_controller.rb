835b0462043135e9449f83e7c9ce0781025be15b
earthctzn <earthctzn1@gmail.com>
2019-10-12 16:06:36 -0400

    Adding create method for sessions#create to log in with facebook.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,29 @@ class SessionsController < ApplicationController
   end
   
   def create
+    binding.pry
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.name = auth['info']['name']
+      u.email = auth['info']['email']
+    end
+
+    session[:user_id] = @user.id
+
+    render 'welcome/home'
   end
+  
 
   def destroy
     session.clear
     redirect_to new_session_path
   end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
+  end
+  
+
 
 end
