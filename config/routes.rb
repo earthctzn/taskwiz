b1c4c2ee6dc9ab46629374d172e7c6c5cb8d57b0
earthctzn <earthctzn1@gmail.com>
2019-10-12 09:45:36 -0400

    Adding welcome_controller with corresponding view, helper, and stylesheet.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :tasks
   
   resources :users do
     resources :tasks, only: [:edit, :show]
   end
-  resources :sessions, only: [:create, :destroy]
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
   
   get '/auth/facebook/callback', to: 'sessions#create'
 
