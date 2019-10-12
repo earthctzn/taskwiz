e0c4525ebda086388b1afa34d7806e7a17b1532b
earthctzn <earthctzn1@gmail.com>
2019-10-12 12:02:04 -0400

    Adding users table and comments table migrations.

---
+++ db/migrate/20191012154928_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.boolean :admin
+      t.datetime :created_at, null: false
+      t.datetime :updated_at, null: false
+    end
+  end
+end
