356d8b80dc850dff41d767104710655d7bb47740
earthctzn <earthctzn1@gmail.com>
2019-10-12 17:30:52 -0400

    Adding description column to tasks table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_12_174257) do
+ActiveRecord::Schema.define(version: 2019_10_12_212715) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_10_12_174257) do
     t.datetime "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|
