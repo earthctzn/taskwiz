b7549cfb34dc89ef984e766734591ecbc23814e1
earthctzn <earthctzn1@gmail.com>
2019-10-12 13:22:17 -0400

    Redoing migrations for practice :)

--- db/migrate/20191012160635_create_tasks.rb
+++
@@ -1,9 +0,0 @@
-class CreateTasks < ActiveRecord::Migration[6.0]
-  def change
-    create_table :tasks do |t|
-      t.string :title
-      t.string :status
-      t.datetime :date
-    end
-  end
-end
