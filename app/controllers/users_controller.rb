831b65051dfb85a5bd58354076806e54b199c295
earthctzn <earthctzn1@gmail.com>
2019-10-12 21:06:18 -0400

    Correcting user_params method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   end
 
   def create
+    binding.pry
     @user = User.new(user_params)
     if @user.save
       redirect_to user_path
@@ -29,6 +30,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :admin)
+    params.permit(:name, :email, :password, :admin)
   end
 end
