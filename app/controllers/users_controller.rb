23c95701b8daf037c67519471c6940be6d460606
earthctzn <earthctzn1@gmail.com>
2019-10-13 00:02:17 -0400

    Removing conditional test from users#show, changing redirect to tasks#new.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,10 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    binding.pry
+
     if @user.save
       session[:user_id] = @user.id
-      redirect_to user_path(@user)
+      redirect_to new_task_path
     else
       redirect_to new_user_path
     end
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
 
   def edit
     logged_in?
-    current_user
+    @user = current_user
   end
 
   def show
