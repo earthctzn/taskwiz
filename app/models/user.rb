db91eaf79caaa587fe05300200b53f722f87331c
earthctzn <earthctzn1@gmail.com>
2019-10-12 20:43:51 -0400

    Correcting validation typos on user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,8 @@ class User < ApplicationRecord
   has_many :tasks
   has_many :comments, through: :tasks
 
-  validates :name, presense: true
+  validates :name, presence: true
   validates :email, uniqueness: true
-  validates :password, presense: true
+  validates :password, presence: true
   validates :password_confirmation, presence: true, on: :create
 end
-s
