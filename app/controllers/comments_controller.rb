8822ca79b055bac2331f7bf0af507d696a8f75d1
earthctzn <earthctzn1@gmail.com>
2019-10-24 03:28:34 -0400

    Stubbing out comments_controller, adding validation to comment model, adding new schema file.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,31 @@
 class CommentsController < ApplicationController
+
+  def new
+  end
+  
+  def create
+  end
+
+  def index
+    if params[:user_id]
+      @user = User.find(params[:user_id])
+      @tasks = @user.tasks
+    else
+      @comments = Comment.all
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    
+  end
+
 end
