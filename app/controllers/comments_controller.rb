75cd4785c9f8d6b98d9e79e8bc4b916509335752
earthctzn <earthctzn1@gmail.com>
2019-10-26 00:27:18 -0400

    Successfully deleting user owned comments. Updating yarn and all gems. Updating navigation link in navbar to user tasks instead of root.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,8 +13,15 @@ class CommentsController < ApplicationController
       @user = current_user
       # binding.pry
       @comment = @user.comments.new(comment_params)
+      @task = Task.find(params[:id])
+      if @comment.save
+        flash[:notice] = "Comment successfully created."
+        redirect_to task_path(@task)
+      end
     else
       @comment = Comment.create(comment_params)
+      flash[:notice] = "Comment successfully created."
+      redirect_to task_path(@task)
     end
   end
 
@@ -31,7 +38,7 @@ class CommentsController < ApplicationController
     if params[:user_id]
       @user = User.find(comment_params[:user_id])
       @task = @user.tasks.find(comment_params[:task_id])
-      @comment = @user.comments.find(comment_params)
+      @comment = @user.comments.find(params[:id])
     else
       @comment = Comment.find_by
     end
@@ -44,13 +51,16 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    self.destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    flash[:notice] = "Comment successfully deleted."
+    redirect_to user_tasks_path(current_user)
   end
 
   private
 
   def comment_params
-    params.require(comment).permit(:content, :user_id, :task_id)
+    params.require(:comment).permit(:content, :user_id, :task_id)
   end
 
 end
