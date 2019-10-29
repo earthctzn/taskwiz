55b680b5e898979446a0b402f961b75848c6412b
earthctzn <earthctzn1@gmail.com>
2019-10-29 02:43:59 -0400

    Renaming scope methods.

--- app/models/task.rb
+++ app/models/task.rb
@@ -10,23 +10,23 @@ class Task < ApplicationRecord
    accepts_nested_attributes_for :comments, reject_if: proc { |attributes| attributes['content'].blank? }
    
 
-   def self.to_do_tasks
-      where(staus: "To do").order(updated_at: :desc)
+   def self.todo
+      where(status: "To do").order(updated_at: :desc)
    end
 
-   def self.in_prog_tasks
-      where(staus: "In Progress").order(updated_at: :desc)
+   def self.in_prog
+      where(status: "In Progress").order(updated_at: :desc)
    end
 
-   def self.ar_tasks
-      where(staus: "Awaiting Response").order(updated_at: :desc)
+   def self.awaiting
+      where(status: "Awaiting Response").order(updated_at: :desc)
    end
 
-   def self.done_tasks
-      where(staus: "Done").order(updated_at: :desc)
+   def self.done
+      where(status: "Done").order(updated_at: :desc)
    end
 
-   def self.completed_today
+   def self.done_today
       where(status: "Done").where(updated_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).order(updated_at: :desc)
    end
 
