c4b44c0c29014d60a8f3d91c25380e79cebb602b
earthctzn <earthctzn1@gmail.com>
2019-10-29 02:06:52 -0400

    Adding scope methods to task model. Fixing bug in task form partial.

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,7 +8,26 @@ class Task < ApplicationRecord
    has_many :users, through: :comments
 
    accepts_nested_attributes_for :comments, reject_if: proc { |attributes| attributes['content'].blank? }
+   
 
+   def self.to_do_tasks
+      where(staus: "To do").order(updated_at: :desc)
+   end
 
+   def self.in_prog_tasks
+      where(staus: "In Progress").order(updated_at: :desc)
+   end
+
+   def self.ar_tasks
+      where(staus: "Awaiting Response").order(updated_at: :desc)
+   end
+
+   def self.done_tasks
+      where(staus: "Done").order(updated_at: :desc)
+   end
+
+   def self.completed_today
+      where(status: "Done").where(updated_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).order(updated_at: :desc)
+   end
 
 end
